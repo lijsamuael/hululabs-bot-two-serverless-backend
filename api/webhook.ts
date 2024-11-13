ee277ed37858959e3e2a51c12ff3256cd14520e0
lijsamuael <lijsamuael@gmail.com>
2024-11-13 20:35:05 +0300

    (fix) correcting the cors

--- api/webhook.ts
+++ api/webhook.ts
@@ -69,8 +69,7 @@ const verifyInitData = (telegramInitData: string, BOT_TOKEN): boolean => {
 const allowCors = (fn) => async (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
